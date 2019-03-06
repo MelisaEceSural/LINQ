f218a6608ec7e4ba81e04c838c83ec4989f96474
MELİSA ECE SURAL <47081681+MelisaEceSural@users.noreply.github.com>
2019-03-06 12:47:04 +0300

    Add files via upload

---
+++ LinqToSqlClasses/LinqToSqlClasses/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace LinqToSqlClasses
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUrunAdii = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbKategori = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbTedarikci = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numFiyat = new System.Windows.Forms.NumericUpDown();
+            this.numStok = new System.Windows.Forms.NumericUpDown();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.txtAra = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numFiyat)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numStok)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 105);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(800, 345);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(23, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ürün Adı";
+            // 
+            // txtUrunAdii
+            // 
+            this.txtUrunAdii.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtUrunAdii.Location = new System.Drawing.Point(26, 30);
+            this.txtUrunAdii.Name = "txtUrunAdii";
+            this.txtUrunAdii.Size = new System.Drawing.Size(100, 24);
+            this.txtUrunAdii.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(383, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Kategori";
+            // 
+            // cmbKategori
+            // 
+            this.cmbKategori.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbKategori.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbKategori.FormattingEnabled = true;
+            this.cmbKategori.Location = new System.Drawing.Point(386, 30);
+            this.cmbKategori.Name = "cmbKategori";
+            this.cmbKategori.Size = new System.Drawing.Size(121, 26);
+            this.cmbKategori.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(532, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 18);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Tedarikçi";
+            // 
+            // cmbTedarikci
+            // 
+            this.cmbTedarikci.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbTedarikci.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbTedarikci.FormattingEnabled = true;
+            this.cmbTedarikci.Location = new System.Drawing.Point(535, 30);
+            this.cmbTedarikci.Name = "cmbTedarikci";
+            this.cmbTedarikci.Size = new System.Drawing.Size(121, 26);
+            this.cmbTedarikci.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(159, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 18);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Fiyat";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(283, 12);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 18);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Stok";
+            // 
+            // numFiyat
+            // 
+            this.numFiyat.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.numFiyat.Location = new System.Drawing.Point(162, 30);
+            this.numFiyat.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numFiyat.Name = "numFiyat";
+            this.numFiyat.Size = new System.Drawing.Size(75, 24);
+            this.numFiyat.TabIndex = 4;
+            // 
+            // numStok
+            // 
+            this.numStok.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.numStok.Location = new System.Drawing.Point(276, 30);
+            this.numStok.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numStok.Name = "numStok";
+            this.numStok.Size = new System.Drawing.Size(75, 24);
+            this.numStok.TabIndex = 4;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEkle.Location = new System.Drawing.Point(26, 60);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(100, 33);
+            this.btnEkle.TabIndex = 5;
+            this.btnEkle.Text = "EKLE";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGuncelle.Location = new System.Drawing.Point(162, 60);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(100, 33);
+            this.btnGuncelle.TabIndex = 5;
+            this.btnGuncelle.Text = "GÜNCELLE";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSil.Location = new System.Drawing.Point(305, 60);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(75, 33);
+            this.btnSil.TabIndex = 5;
+            this.btnSil.Text = "SİL";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // txtAra
+            // 
+            this.txtAra.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtAra.Location = new System.Drawing.Point(688, 32);
+            this.txtAra.Name = "txtAra";
+            this.txtAra.Size = new System.Drawing.Size(100, 24);
+            this.txtAra.TabIndex = 2;
+            this.txtAra.TextChanged += new System.EventHandler(this.txtAra_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(724, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 18);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Ürün Ara";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.numStok);
+            this.Controls.Add(this.numFiyat);
+            this.Controls.Add(this.cmbTedarikci);
+            this.Controls.Add(this.cmbKategori);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtAra);
+            this.Controls.Add(this.txtUrunAdii);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numFiyat)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numStok)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUrunAdii;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbKategori;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbTedarikci;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numFiyat;
+        private System.Windows.Forms.NumericUpDown numStok;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.TextBox txtAra;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ LinqToSqlClasses/LinqToSqlClasses/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LinqToSqlClasses
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        NorthwindDataContext data = new NorthwindDataContext();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //NorthwindDataContext data = new NorthwindDataContext();
+            dataGridView1.DataSource = data.Urunlers.Where(x=>x.Sonlandi==false);// burada bir select sorgusu çalıştı.
+
+            #region Joinli Hali
+            //dataGridView1.DataSource = (from urun in data.Urunlers
+            //                           join kategori in data.Kategorilers
+            //                           on urun.KategoriID equals kategori.KategoriID
+            //                           join tedarikci in data.Tedarikcilers
+            //                           on urun.TedarikciID equals tedarikci.TedarikciID
+            //                            select new
+            //                            {
+            //                                ID = urun.UrunID,
+            //                                Adi = urun.UrunAdi,
+            //                                Fiyat = urun.BirimFiyati,
+            //                                Stok = urun.HedefStokDuzeyi,
+            //                                Kategori = kategori.KategoriAdi,
+            //                                Tedarikci = tedarikci.SirketAdi
+            //                            }).OrderBy(x => x.Adi); 
+            #endregion
+
+            cmbKategori.DataSource = data.Kategorilers;
+            cmbKategori.DisplayMember = "KategoriAdi";
+            cmbKategori.ValueMember = "KategoriID";
+
+            cmbTedarikci.DataSource = data.Tedarikcilers;
+            cmbTedarikci.DisplayMember = "SirketAdi";
+            cmbTedarikci.ValueMember = "TedarikciID";
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(txtUrunAdii.Text) || 
+                numFiyat.Value==0 || numStok.Value == 0)
+            {
+                MessageBox.Show("Lütfen alanları boş bırakmayın.");
+                return;
+            }
+
+
+            Urunler yeniUrun = new Urunler();
+            yeniUrun.UrunAdi = txtUrunAdii.Text;
+            yeniUrun.BirimFiyati = numFiyat.Value;
+            yeniUrun.HedefStokDuzeyi = (short)numStok.Value;
+            yeniUrun.KategoriID = (int)cmbKategori.SelectedValue;
+            yeniUrun.TedarikciID = (int)cmbTedarikci.SelectedValue;
+
+            //önce dataContext'e yeni ürünü ekleyelim
+            //NorthwindDataContext data = new NorthwindDataContext();
+            data.Urunlers.InsertOnSubmit(yeniUrun);
+            // yeni ürünü contexte ekledik, şimdi de veritabanına yazalım.
+            data.SubmitChanges();
+
+            //değişikliği görmek için ürünleri tekrar listeleyelim
+            dataGridView1.DataSource = data.Urunlers.Where(x => x.Sonlandi == false);
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow == null) return;
+
+            //NorthwindDataContext data = new NorthwindDataContext();
+            int id = (int)dataGridView1.CurrentRow.Cells["UrunID"].Value;
+            Urunler silinecekUrun = data.Urunlers.SingleOrDefault(x => x.UrunID == id);
+            data.Urunlers.DeleteOnSubmit(silinecekUrun);
+            data.SubmitChanges();
+            dataGridView1.DataSource = data.Urunlers.Where(x => x.Sonlandi == false);
+        }
+
+        private void txtAra_TextChanged(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = data.Urunlers.Where(x => x.UrunAdi.Contains(txtAra.Text));
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow satir = dataGridView1.CurrentRow;
+            txtUrunAdii.Text = satir.Cells["UrunAdi"].Value.ToString();
+            txtUrunAdii.Tag = (int)satir.Cells["UrunID"].Value;
+            numFiyat.Value = (decimal)satir.Cells["BirimFiyati"].Value;
+            numStok.Value = (short)satir.Cells["HedefStokDuzeyi"].Value;
+            cmbKategori.SelectedValue = (int)satir.Cells["KategoriID"].Value;
+            cmbTedarikci.SelectedValue = (int)satir.Cells["TedarikciID"].Value;
+            
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            if(txtUrunAdii.Tag==null)
+            {
+                MessageBox.Show("Güncellenecek ürün bulunamadı.");
+                return;
+            }
+
+            int id = (int)txtUrunAdii.Tag;
+            Urunler urun = data.Urunlers.SingleOrDefault(x => x.UrunID == id);
+            urun.UrunAdi = txtUrunAdii.Text;
+            urun.BirimFiyati = numFiyat.Value;
+            urun.HedefStokDuzeyi = (short)numStok.Value;
+            urun.KategoriID = (int)cmbKategori.SelectedValue;
+            urun.TedarikciID = (int)cmbTedarikci.SelectedValue;
+
+            data.SubmitChanges();
+
+            dataGridView1.DataSource = data.Urunlers.Where(x => x.Sonlandi == false);
+            txtUrunAdii.Tag = null;
+        }
+    }
+}
